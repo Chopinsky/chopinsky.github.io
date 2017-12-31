bd9c3882c2dfe7aaac9f70ada22e85b587169bd3
Jacob Zuo <chopinsky@live.com>
2017-12-30 18:16:05 -0600

    splitting the view

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+import Header from './Header';
+
+export default class Content extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the main content</p>
+      </div>
+    );
+  }
+}
