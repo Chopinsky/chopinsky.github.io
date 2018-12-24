18b1caf84127419106bfb6d8b83635a55503fb1a
Jacob Zuo <chopinsky@live.com>
2018-12-23 23:00:20 -0600

    Updates

---
+++ precache-manifest.90423d5a4208a97fc475a8531b858f6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96c493b4c9021b108313",
+    "url": "/static/js/main.96c493b4.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "96c493b4c9021b108313",
+    "url": "/static/css/main.7c1393b7.chunk.css"
+  },
+  {
+    "revision": "0bb2e55002bbf4a26ff28cdca7ef5ab7",
+    "url": "/index.html"
+  }
+];
