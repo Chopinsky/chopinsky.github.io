16fba9d7481684cb4ac9030bffb7b433bc491c4d
Jacob Zuo <chopinsky@live.com>
2018-12-23 22:35:45 -0600

    adding back missing files

---
+++ build/precache-manifest.581213fca516bfd43d419754175a1c23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d312271056990a28dcaa",
+    "url": "/static/js/main.d3122710.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d312271056990a28dcaa",
+    "url": "/static/css/main.c27f21b4.chunk.css"
+  },
+  {
+    "revision": "e62d08afc55b50549d63b242397d17c5",
+    "url": "/index.html"
+  }
+];
